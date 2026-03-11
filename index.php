245ad5dc71aee93dfac243a830b755e0535eca89
Arnoud2208 <arnoudvoeten09@gmail.com>
2026-03-11 11:38:51 +0100

    Inlogpagina

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="nl">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Inloggen</title>
+
+    <link rel="stylesheet" href="login.css">
 </head>
+
 <body>
     
+<div class="container">
+
+    <form class="login-form" action="login.php" method="POST">
+
+        <h2>Inloggen</h2>
+
+        <div class="form-group">
+            <label for="username">Gebruikersnaam</label>
+            <input type="text" name="username" id="username" required>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Wachtwoord</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+
+        <button type="submit">Inloggen</button>
+
+    </form>
+
+</div>
+
 </body>
+
 </html>
