f473c75a647698645083312934ad7d5efc2cabc4
kaoutarelmassoudi14-commits <kaoutarelmassoudi14@gmail.com>
2026-03-18 12:31:23 +0100

    Taak toevoegen verbeterd 2

--- backend/add_task.php
+++ backend/add_task.php
@@ -4,10 +4,10 @@ require_once 'conn.php';
 $titel = $_POST['titel'];
 $beschrijving = $_POST['beschrijving'];
 $status = $_POST['status'];
-$afdeling = $_POST['status']
+$afdeling = $_POST['afdeling'];
 
 $query = "INSERT INTO taken (titel, beschrijving, afdeling, status) 
-        VALUES (:titel, :beschrijving, 'algemeen', :status)";
+          VALUES (:titel, :beschrijving, :afdeling, :status)";
 
 $stmt = $conn->prepare($query);
 $stmt->execute([
@@ -15,7 +15,6 @@ $stmt->execute([
     ':beschrijving' => $beschrijving,
     ':status' => $status,
     ':afdeling' => $afdeling
-
 ]);
 
 header("Location: ../doc/home.php");
